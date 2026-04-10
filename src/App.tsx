cea14413612e815534be17518f4d32b4af94a26c
Colin Reinhardt <reinhardtc22@gmail.com>
2026-04-10 12:26:14 -0400

    added coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Layout from "./components/Layout";
-import HomePage from "./components/HomePage";
+// import Layout from "./components/Layout";
+// import HomePage from "./components/HomePage";
 
 function App() {
   return (
-    <Layout>
-      <HomePage />
-    </Layout>
+    <div className="text-(--chocolate-rust) text-5xl font-extrabold font-syne mx-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-(--evergreen) p-5">
+      Coming Soon
+    </div>
   );
 }
 
